9c0b5db3c6eae7e1b0c005711087389230dfe561
aluya ehis <aluyaehis8@gmail.com>
2025-04-25 17:49:34 +0100

    update database test

--- DjangoCrud/settings.py
+++ DjangoCrud/settings.py
@@ -81,24 +81,24 @@ WSGI_APPLICATION = 'DjangoCrud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Employee-Management-system',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'PORT': '5432',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'Employee-Management-system',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'PORT': '5432',
+#         'HOST': 'localhost'
+#     }
+# }
 
 
 # Password validation
